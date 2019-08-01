6cbb54e1e91437a5bfd125b2a2a001082a7ee267
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-08-01 20:21:27 +0200

    Initial Commit

---
+++ NewTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewTable
+//
+//  Created by Ilija Mihajlovic on 8/1/19.
+//  Copyright © 2019 Ilija Mihajlovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
