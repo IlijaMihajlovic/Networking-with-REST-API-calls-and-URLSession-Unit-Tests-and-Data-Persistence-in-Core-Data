32293ed717ff5616003fccc4b69acd6faf181dfa
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-08-03 18:14:48 +0200

    added CoreData

---
+++ NewTable/PersistenceService.swift
@@ -0,0 +1,49 @@
+//
+//  PersistenceService.swift
+//  NewTable
+//
+//  Created by Ilija Mihajlovic on 8/3/19.
+//  Copyright © 2019 Ilija Mihajlovic. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PersistenceService {
+
+    private init() {}
+    static let shared = PersistenceService()
+
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "coredra")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func save() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
