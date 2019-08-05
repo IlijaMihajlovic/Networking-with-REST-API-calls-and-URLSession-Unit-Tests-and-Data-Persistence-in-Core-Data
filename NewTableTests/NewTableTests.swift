0a3335d069d20c57595ffd4027ff9dde9855c4aa
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-08-05 18:25:25 +0200

    Add unit test to test valid call to JSONPlaceholder server

--- NewTableTests/NewTableTests.swift
+++ NewTableTests/NewTableTests.swift
@@ -8,28 +8,49 @@
 
 import XCTest
 @testable import NewTable
+
 class NewTableTests: XCTestCase {
 
     var sut: URLSession!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sut = URLSession(configuration: .default)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testValidCallToJSONPlaceholer() {
+        // given
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+
+        //What we expect to happen
+        let promise = expectation(description: "Status code: 200 or 201")
+
+        // when
+        let dataTask = sut.dataTask(with: url!) { data, response, error in
+
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+
+                if statusCode == 200 || statusCode == 201 {
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
         }
+        dataTask.resume()
+
+        //Keeps the test running until all expectations are fulfilled, or the timeout interval ends, whichever happens first
+        wait(for: [promise], timeout: 5)
     }
 
 }
